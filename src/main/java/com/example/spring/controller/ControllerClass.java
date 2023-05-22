42b8a74c757b890f74d526cbfbb98590728fa21f
Dilli Babu <104541530+Dillibabu390@users.noreply.github.com>
2023-05-22 13:39:36 +0530

    Add files via upload

---
+++ src/main/java/com/example/spring/controller/ControllerClass.java
@@ -0,0 +1,14 @@
+package com.example.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerClass {
+
+    @GetMapping("/get")
+    public String get(){
+        return "hello api";
+    }
+
+}
